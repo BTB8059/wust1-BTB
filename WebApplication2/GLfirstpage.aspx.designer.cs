e95c1e9499a98f92b35534160d9fac9a9cb84dc0
zhaojie1234 <986162980@qq.com>
2017-07-06 11:20:01 +0800

    zj

--- WebApplication2/GLfirstpage.aspx.designer.cs
+++ WebApplication2/GLfirstpage.aspx.designer.cs
@@ -22,6 +22,24 @@ namespace WebApplication2 {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
+        /// lable1 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lable1;
+        
+        /// <summary>
+        /// lbl1 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lbl1;
+        
+        /// <summary>
         /// Label2 控件。
         /// </summary>
         /// <remarks>
